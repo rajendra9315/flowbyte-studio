93bda6aeed3ebdc023cba10c7ed48b89526d468c
Rajendra 5 <rawatrajendra9315@gmail.com>
2025-12-13 11:00:55 +0530

    Add lightweight hero text slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,20 @@ document.querySelectorAll(".accordion-header").forEach(button => {
   });
 });
 
+// Hero text slider
+const heroTexts = [
+  "Web, Ads & Automation Made Simple",
+  "Everything Digital, One Place",
+  "Professional Services for Modern Businesses"
+];
+
+let heroIndex = 0;
+const heroHeading = document.querySelector(".hero h1");
+
+if (heroHeading) {
+  setInterval(() => {
+    heroIndex = (heroIndex + 1) % heroTexts.length;
+    heroHeading.innerHTML = heroTexts[heroIndex].replace(" Made Simple", "<br><span>Made Simple</span>");
+  }, 3000);
+}
+
