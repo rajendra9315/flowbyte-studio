438c0f42c4b8ac693fe3b98fbbab02886d1aef1c
Rajendra 5 <rawatrajendra9315@gmail.com>
2025-12-14 21:05:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,3 +89,33 @@ if (carousel) {
   });
 }
 
+const slider = document.querySelector('.carousel-track');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+if (slider) {
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5;
+    slider.scrollLeft = scrollLeft - walk;
+  });
+}
+
